2a3336491c7e97f426a66d18821f51f299a27038
uronico <uronico3@gmail.com>
2025-03-10 01:36:38 +0800

    update changes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-//router.get('/', (req,res) => { res.send('Hello World');});
+router.get('/', (req,res) => { res.send('Hello World');});
 
 router.use('/users', require('./users'));
 
